b4d48f5ddc8a3fdd6a2a035a8b98b5c48fd9efae
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-05-17 14:44:45 +0530

    fix : cors

--- server/app.js
+++ server/app.js
@@ -21,7 +21,10 @@ app.use(express.urlencoded({ extended: true }));
 // third party middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://pizza-app-alpha-pink.vercel.app/",
+    ],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
